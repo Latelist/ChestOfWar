79764abe9af692309b44c5fc41e53325913b643b
Salatchen <pozhivi.s.moe@gmail.com>
2024-11-23 00:33:16 +0300

    Страница game теперь отображает инфу о юните. Также на ней есть кнопка «к игре», которая ведет на новую страницу с доской. Шахматная доска умеет реагировать на положение мышки и нажатия на ячейки

--- src/main/java/com/example/ChestOfWar/сontroller/ChestOfWarController.java
+++ src/main/java/com/example/ChestOfWar/сontroller/ChestOfWarController.java
@@ -3,12 +3,14 @@ package com.example.ChestOfWar.сontroller;
 import com.example.ChestOfWar.model.units.Archer;
 import com.example.ChestOfWar.model.units.Unit;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller //класс, который отвечает за прием HTTP-запросов
 public class ChestOfWarController {
-    @GetMapping //функция с такой аннотацией возвращает шаблон, который соответствует имени строки
-    public String game() {
+    @GetMapping("/game") //функция с такой аннотацией возвращает шаблон, который соответствует имени строки
+    public String game(Model model) {
+
 
         Unit archer1 = new Archer("archer1", new int[]{0, 0});
         System.out.println(archer1.toString());
@@ -18,7 +20,13 @@ public class ChestOfWarController {
 
         archer2.attack(archer1);
         System.out.println(archer1);
+
+        model.addAttribute("unit", archer1);
         return "game";
+    }
+    @GetMapping("/game-board")
+    public String showGameBoard() {
+        return "game-board";
     }
 
 
